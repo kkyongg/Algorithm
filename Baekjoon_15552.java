095736500ee74a0d23d4d8032716e9b46a5ced33
kyongminkim <kmkim1319@gmail.com>
2023-03-02 14:35:06 +0900

    빠른A+B_0302

---
+++ Baekjoon_15552.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.io.*;
+
+public class Baekjoon_15552 {
+    public static void main(String[] args) throws IOException {
+
+        //Scanner 와 System.out.println 대신
+        //BufferedReader 와 BufferedWriter 를 사용
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader((System.in)));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        int testCase = Integer.parseInt(bufferedReader.readLine());
+
+        String result;
+        for (int i = 0; i < testCase; i++) {
+
+            //두 수를 입력을 공백으로 구분하기
+            String[] input = bufferedReader.readLine().split(" ");
+            int n1 = Integer.parseInt(input[0]);
+            int n2 = Integer.parseInt(input[1]);
+            result = String.valueOf(n1 + n2);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+        bufferedWriter.flush();
+    }
+}
