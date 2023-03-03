dfc7c0cb21ad73042ae9689f193d85e6b87f657c
kyongminkim <kmkim1319@gmail.com>
2023-03-03 16:13:07 +0900

    단어 길이 재기_0303

---
+++ Baekjoon_2743.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon_2743 {
+    public static void main(String[] args) {
+
+        //문자열 입력받아서 문자열 길이 재기
+        Scanner str = new Scanner(System.in);
+
+        String a = str.next();
+        int b = a.length();
+
+        System.out.println(b);
+    }
+}
