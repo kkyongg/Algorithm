021a7fd5189f989aa591761ef8940bca71e935e5
kyongmin <kkyongg2@gmail.com>
2023-03-03 16:12:41 +0900

    합_0303

---
+++ Baekjoon_8393.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon_8393 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int sum = 0;
+        for (int i = 1; i < num + 1; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
