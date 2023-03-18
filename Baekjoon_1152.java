6160395c7a269d04b9c4abde6340f711919f0f5d
kyongmin <kkyongg2@gmail.com>
2023-03-19 00:04:17 +0900

    단어의 개수_0319

---
+++ Baekjoon_1152.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+//단어의 개수
+public class Baekjoon_1152 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.nextLine();
+        StringTokenizer stringTokenizer = new StringTokenizer(str," ");
+
+        System.out.println(stringTokenizer.countTokens());
+
+    }
+}
