8bf499a08efe2b4280332bb6ecf008cb786b515c
kyongmin <110990435+kkyongg@users.noreply.github.com>
2023-04-02 21:05:04 +0800

    스택_0402

---
+++ Baekjoon_10828.java
@@ -0,0 +1,49 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Baekjoon_10828 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+
+        //push, pop, size, empty, top
+        int testCase = Integer.parseInt(bufferedReader.readLine());
+        StringTokenizer stringTokenizer;
+
+        for (int i = 0; i < testCase; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            String order = stringTokenizer.nextToken();
+
+            if (order.equals("push")) {
+                stack.push(Integer.parseInt(stringTokenizer.nextToken()));
+            }
+            if (order.equals("pop")) {
+                if (stack.isEmpty()) {
+                    System.out.println(-1);
+                } else {
+                    System.out.println(stack.pop());
+                }
+            }
+            if (order.equals("top")) {
+                if (stack.isEmpty()) {
+                    System.out.println(-1);
+                } else {
+                    System.out.println(stack.peek());
+                }
+            }
+            if (order.equals("empty")) {
+                if (stack.isEmpty()) {
+                    System.out.println(1);
+                } else {
+                    System.out.println(0);
+                }
+            }
+            if (order.equals("size")) System.out.println(stack.size());
+        }
+    }
+}
