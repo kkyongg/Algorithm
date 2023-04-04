0fc08844d122301e87718170aa5f1a1dc69c56d1
kyongmin <110990435+kkyongg@users.noreply.github.com>
2023-04-04 13:58:50 +0800

    회사에 있는 사람_0404

---
+++ Baekjoon_7785.java
@@ -0,0 +1,37 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Baekjoon_7785 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int recordCase = Integer.parseInt(bufferedReader.readLine());
+
+        StringTokenizer stringTokenizer;
+        Map<String, String> map = new HashMap<>();
+        for (int i = 0; i < recordCase; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+            String name = stringTokenizer.nextToken();
+            String situation = stringTokenizer.nextToken();
+            map.put(name, situation);
+
+            if (map.containsKey(name) && situation.equals("leave")) {
+                map.remove(name);
+            }
+        }
+
+        //key 정렬
+        List<String> keyList = new ArrayList<>(map.keySet());
+        Collections.sort(keyList);
+
+        //key 역순으로 출력
+        for (int i = keyList.size() - 1; i > -1; i--) {
+            System.out.println(keyList.get(i));
+        }
+    }
+}
