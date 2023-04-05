8cb7545ce5ffc4680303508aa734b1f0a25261f9
kyongmin <110990435+kkyongg@users.noreply.github.com>
2023-04-05 22:56:13 +0800

    그대로 출력하기_0405

---
+++ Baekjoon_11718.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Baekjoon_11718 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            System.out.println(scanner.nextLine());
+        }
+    }
+}
