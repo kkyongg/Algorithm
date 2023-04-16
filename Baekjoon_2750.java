c245d254e57cea3fd570326696eaaa8e2c01ed84
kyongmin <110990435+kkyongg@users.noreply.github.com>
2023-04-16 23:14:34 +0800

    수 정렬하기_0416

---
+++ Baekjoon_2750.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Baekjoon_2750 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(bufferedReader.readLine());
+
+        int[] numArr = new int[input];
+        for (int i = 0; i < input; i++) {
+            numArr[i] = Integer.parseInt(bufferedReader.readLine());
+
+        }
+        Arrays.sort(numArr);
+        for (int i = 0; i < numArr.length; i++) {
+            System.out.println(numArr[i]);
+        }
+    }
+}
