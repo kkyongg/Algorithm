0f0f99d4e13a0c200690791b1fe5e6a6a8525116
kyongmin <110990435+kkyongg@users.noreply.github.com>
2023-04-18 21:44:53 +0800

    팩토리얼_0418

--- Baekjoon_10872.java
+++ Baekjoon_10872.java
@@ -6,11 +6,11 @@ import java.io.InputStreamReader;
 
 public class Baekjoon_10872 {
     public static void main(String[] args) throws IOException {
-        //재귀함수 사용해서 코드 짜보기
+        
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(bufferedReader.readLine());
-
+/*
         if (n == 0) {
             System.out.println(1);
         } else {
@@ -22,5 +22,15 @@ public class Baekjoon_10872 {
             }
             System.out.println(sum);
         }
+*/        
+        //재귀함수 사용해서 코드 짜보기
+        int sum = factorial(n);
+        System.out.println(sum);
+        
+    }
+    public static int factorial(int n) {
+        if (n == 1)
+            return 1;
+        return n * factorial(n - 1);
     }
 }
