9013ac6f237b78542e7cd5084253677a229bb61f
kyongmin <110990435+kkyongg@users.noreply.github.com>
2023-04-19 22:50:05 +0800

    X보다 작은 수_0419

---
+++ Baekjoon_10871.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_10871 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        int n = Integer.parseInt(stringTokenizer.nextToken());
+        int x = Integer.parseInt(stringTokenizer.nextToken());
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 0; i < n; i++) {
+            int input = Integer.parseInt(stringTokenizer.nextToken());
+            if(input<x){
+                stringBuilder.append(input + " ");
+            }
+        }
+        System.out.println(stringBuilder);
+    }
+}
