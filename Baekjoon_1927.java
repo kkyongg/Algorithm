d7a5a18280594ce4abd5ebf20d3a245c95560dd4
kyongmin <110990435+kkyongg@users.noreply.github.com>
2023-04-20 21:00:24 +0800

    최소 힙_0420

---
+++ Baekjoon_1927.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Baekjoon_1927 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int inputNum = Integer.parseInt(bufferedReader.readLine());
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int i = 0; i < inputNum; i++) {
+            int input = Integer.parseInt(bufferedReader.readLine());
+            if (input == 0) {
+                if (queue.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(queue.poll());
+                }
+            } else {
+                queue.add(input);
+            }
+        }
+    }
+}
