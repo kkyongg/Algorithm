5697428dada08a0b9f2c58ed6f0ac5e5d689a5cd
kyongmin <110990435+kkyongg@users.noreply.github.com>
2023-04-24 22:28:54 +0800

    배수와 약수_0424

---
+++ Baekjoon_5086.java
@@ -0,0 +1,32 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_5086 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer stringTokenizer;
+        for (int i = 0; i < 1000; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+            int first = Integer.parseInt(stringTokenizer.nextToken());
+            int last = Integer.parseInt(stringTokenizer.nextToken());
+
+            if (first == 0 && last == 0) {
+                break;
+            } else if (last % first == 0) {
+                System.out.println("factor");
+            } else if (first % last == 0) {
+                System.out.println("multiple");
+            } else {
+                System.out.println("neither");
+            }
+
+        }
+
+    }
+}
