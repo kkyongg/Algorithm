422ae48eae43fcdbe5c6faca927318857658d510
kkyongg <kkyongg2@gmail.com>
2023-06-22 23:59:49 +0900

    0622

---
+++ BOJ/Main_10866_덱.java
@@ -0,0 +1,73 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main_10866_덱 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        Deque<Integer> dq = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+
+            switch (str) {
+                case "push_front":
+                    dq.addFirst(Integer.parseInt(st.nextToken()));
+                    break;
+                case "push_back":
+                    dq.addLast(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop_front":
+                    if (dq.isEmpty()) {
+                        sb.append(-1).append('\n');
+                    } else {
+                        sb.append(dq.pollFirst()).append('\n');
+                    }
+                    break;
+                case "pop_back":
+                    if (dq.isEmpty()) {
+                        sb.append(-1).append('\n');
+                    } else {
+                        sb.append(dq.pollLast()).append('\n');
+                    }
+                    break;
+                case "size":
+                    sb.append(dq.size()).append('\n');
+                    break;
+                case "empty":
+                    if (dq.isEmpty()) {
+                        sb.append(1).append('\n');
+                    } else {
+                        sb.append(0).append('\n');
+                    }
+                    break;
+                case "front":
+                    if (dq.isEmpty()) {
+                        sb.append(-1).append('\n');
+                    } else {
+                        sb.append(dq.peekFirst()).append('\n');
+                    }
+                    break;
+                case "back":
+                    if (dq.isEmpty()) {
+                        sb.append(-1).append('\n');
+                    } else {
+                        sb.append(dq.peekLast()).append('\n');
+                    }
+                    break;
+            }
+
+        }
+        System.out.println(sb);
+    }
+}
