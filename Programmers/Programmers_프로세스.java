666e60745c3b50aaedd80d242dcc2b97338fb8be
kkyongg <kkyongg2@gmail.com>
2023-08-02 01:16:33 +0900

    0802

--- Programmers/Programmers_프로세스.java
+++ Programmers/Programmers_프로세스.java
@@ -7,7 +7,6 @@ public class Programmers_프로세스 {
 
         System.out.println(solution(new int[]{2, 1, 3, 2}, 2));
         System.out.println(solution(new int[]{1, 1, 9, 1, 1, 1}, 0));
-        
     }
 
     public static int solution(int[] priorities, int location) {
