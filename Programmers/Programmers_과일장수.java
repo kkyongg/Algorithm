359b88d1a89b061998fd35679bc13a46c53457e6
kkyongg <kkyongg2@gmail.com>
2023-08-13 17:56:39 +0900

    0813

---
+++ Programmers/Programmers_과일장수.java
@@ -0,0 +1,27 @@
+package Programmers;
+
+import java.util.Arrays;
+
+public class Programmers_과일장수 {
+    public static void main(String[] args) {
+        System.out.println(solution(3, 4, new int[]{1, 2, 3, 1, 2, 3, 1}));
+        System.out.println(solution(4, 3, new int[]{4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2}));
+    }
+
+    public static int solution(int k, int m, int[] score) {
+        int answer = 0;
+
+        if (score.length < m) {
+            return 0;
+        }
+        Arrays.sort(score);
+
+        for (int i = score.length - m; i >= 0; i -= m) {
+            answer += score[i] * m;
+        }
+
+        return answer;
+    }
+
+}
+
