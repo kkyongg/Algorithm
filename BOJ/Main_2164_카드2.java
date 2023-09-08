bc2a4b927627158e0741fa38eb4244f6cd44d205
kkyongg <kkyongg2@gmail.com>
2023-09-08 22:18:05 +0900

    0908

---
+++ BOJ/Main_2164_카드2.java
@@ -0,0 +1,26 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main_2164_카드2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int card = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < card; i++) {
+            queue.offer(i + 1);
+        }
+
+        while (queue.size() != 1) {
+            queue.poll();
+            queue.offer(queue.poll());
+        }
+
+        System.out.println(queue.poll());
+    }
+}
