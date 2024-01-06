456167d17bc83f6cf9f9061e4ea0a155605e371f
kkyongg <kkyongg2@gmail.com>
2024-01-07 00:20:02 +0900

    0107

--- BOJ/Main_11478_서로다른부분문자열의개수.java
+++ BOJ/Main_11478_서로다른부분문자열의개수.java
@@ -12,10 +12,26 @@ public class Main_11478_서로다른부분문자열의개수 {
         String str = br.readLine();
         Set<String> set = new HashSet<>();
 
+        //1번 풀이
         for (int i = 0; i < str.length(); i++) {
             for (int j = i + 1; j <= str.length(); j++) {
                 set.add(str.substring(i, j));
             }
+        }
+
+        //2번 풀이
+        int count = 0;
+        int split = 1;
+        while (count != str.length()) {
+            for (int i = 0; i < str.length(); i++) {
+                if (i + split == str.length() + 1) {
+                    break;
+                } else {
+                    set.add(str.substring(i, i + split));
+                }
+            }
+            split++;
+            count++;
         }
 
         System.out.println(set.size());
