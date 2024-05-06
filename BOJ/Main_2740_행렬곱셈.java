6ab72ffb49db1a9647c9e5105542f9dc041d13f0
kkyongg <kkyongg2@gmail.com>
2024-05-06 17:49:54 +0900

    0506

---
+++ BOJ/Main_2740_행렬곱셈.java
@@ -0,0 +1,60 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_2740_행렬곱셈 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        //행렬 A : N x M
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        //행렬 A 입력
+        int[][] A = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        //B : M x K
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        //행렬 B 입력
+        int[][] B = new int[M][K];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < K; j++) {
+                B[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        //행렬 A * B 계산
+        int[][] result = new int[N][K];
+
+        for (int i = 0; i < N; i++) { //A의 행
+            for (int j = 0; j < K; j++) { //B의 열
+                for (int k = 0; k < M; k++) {
+                    result[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < K; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
